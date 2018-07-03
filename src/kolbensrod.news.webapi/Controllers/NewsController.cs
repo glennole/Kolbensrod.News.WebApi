5538289d91c7667837f21e3d311f187a5ca84217
Glenn Haugen <glenn.ole.haugen@gmail.com>
2018-07-04 00:23:59 +0200

    Initial commit

---
+++ src/kolbensrod.news.webapi/Controllers/NewsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace kolbensrod.news.webapi.Controllers
+{
+    [Route("api/[controller]")]
+    public class NewsController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<Models.News> Get()
+        {
+            var n1 = new Models.News()
+            {
+                NewsId = 1,
+                Title = "Ny nettside",
+                Text = "Vi har nå fått oss ny nettside. Følg med og få med deg alt som skjer!",
+                PublishedDate = new DateTime(2018, 04, 25)
+
+            };
+            var n2 = new Models.News()
+            {
+                NewsId = 2,
+                Title = "Nytt adgangssystem",
+                Text = "Vi har nå fått oss nytt adgangssystem. Ta turen oppom resepsjonen for å bytte til nytt kort!",
+                PublishedDate = new DateTime(2018, 04, 25)
+
+            };
+            var news = new List<Models.News>();
+            news.Add(n1);
+            news.Add(n2);
+            return news;
+        }
+    }
+}
---
+++ src/kolbensrod.news.webapi/Models/News.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace kolbensrod.news.webapi.Models
+{
+    public class News
+    {
+        public int NewsId { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public bool Active { get; set; }
+    }
+}
